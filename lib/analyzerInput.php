e7eefc17cee08221ce0393ad42c483270dd853b0
Daniel Pérez <plupyt_dany@hotmail.com>
2021-10-03 19:14:16 -0500

    restructure analyzerInput

--- lib/analyzerInput.php
+++ lib/analyzerInput.php
@@ -1,126 +1,19 @@
 <?php
     include("databaseFunctions.php");
     include("printResults.php");
+    include("structureQueryWithCampos.php");
+    include("structureQueryWithoutCampos.php");
 
     function analyzerInput($words) {
         //Detección de campos
         $camposInput = lookCamposInput($words);
         $hasCamposInput = $camposInput ? true : false;
-        if($camposInput) {
-            $words = deleteCamposFromWords($words);
-        }
         
         if($hasCamposInput) {
-            withCampos($words, $camposInput);
+            $words = deleteCamposFromWords($words);
+            structureQueryWithCampos($words, $camposInput);
         } else {
-            withoutCampos($words);
-        }
-    }
-
-    function withCampos($words, $camposInput) {
-        $camposArray = explode(",", $camposInput);
-        foreach($camposArray as $campo) {
-            $temp = explode(".", $campo);
-            $tableToSearch = $temp[0];
-            $campoToSearch = $temp[1];
-        }
-
-        $query = "SELECT " . $camposInput . " FROM " . $tableToSearch . " WHERE ";
-        for ($j=0; $j < count($words); $j++) { 
-            switch ($words[$j]) {
-                case "AND":
-                    $query .= " AND ";
-                    break;
-                case "OR":
-                    $query .= " OR ";
-                    break;
-                case "NOT":
-                    $query .= "NOT ";
-                    break; 
-                default:
-                    switch ( strstr($words[$j], '(', true) ) {
-                        case 'CADENA':
-                            //echo "encontré una cadena()";
-                            if(strpos($words[$j], ")")) {
-                                $wordToSearch = substr(strstr($words[$j], '('), 1, -1);
-                                $query .= $campoToSearch . " = '" . $wordToSearch . "'";
-                            } else {
-                                $wordToSearch = substr(strstr($words[$j], '('), 1); //elimina caracter "("
-                                while(!strpos($words[$j], ")")) {
-                                    $j++;
-                                    $wordToSearch .= " " . $words[$j];
-                                }
-                                $wordToSearch = substr($wordToSearch, 0 , -1); //elimina caracter ")"
-                                $query .= $campoToSearch . " = '" . $wordToSearch . "'";
-                            }
-                            break;
-                        case 'PATRON':
-                            //echo "encontré un patrón()";
-                            $wordToSearch = substr(strstr($words[$j], '('), 1, -1);
-                            $query .= $campoToSearch . " LIKE '%" . $wordToSearch . "%'";
-                            break;
-                        default:
-                            //echo "encontré una palabra";
-                            $query .= $campoToSearch . " LIKE '%" . $words[$j] . "%'";
-                            break;
-                    }
-                    break;
-            }
-        }
-        echo $query . "<br/><br/>";
-        $results = executeQuery($query);
-        printResults($results);
-    }
-
-    function withoutCampos($words) {
-        $categoriasBusqueda = ["ProductName", "QuantityPerUnit", "CategoryID"];
-        $tableToSearch = "products";
-        for ($i=0; $i < count($categoriasBusqueda); $i++) {
-            $query = "SELECT " . "products.ProductName, products.QuantityPerUnit, products.CategoryID" . " FROM " . $tableToSearch . " WHERE ";
-            for ($j=0; $j < count($words); $j++) { 
-                switch ($words[$j]) {
-                    case "AND":
-                        $query .= " AND ";
-                        break;
-                    case "OR":
-                        $query .= " OR ";
-                        break;
-                    case "NOT":
-                        $query .= "NOT ";
-                        break; 
-                    default:
-                        switch ( strstr($words[$j], '(', true) ) {
-                            case 'CADENA':
-                                //echo "encontré una cadena()";
-                                if(strpos($words[$j], ")")) {
-                                    $wordToSearch = substr(strstr($words[$j], '('), 1, -1);
-                                    $query .= $categoriasBusqueda[$i] . " = '" . $wordToSearch ."'";
-                                } else {
-                                    $wordToSearch = substr(strstr($words[$j], '('), 1); //elimina caracter "("
-                                    while(!strpos($words[$j], ")")) {
-                                        $j++;
-                                        $wordToSearch .= " " . $words[$j];
-                                    }
-                                    $wordToSearch = substr($wordToSearch, 0 , -1); //elimina caracter ")"
-                                    $query .= $categoriasBusqueda[$i] . " = '" . $wordToSearch . "'";
-                                }
-                                break;
-                            case 'PATRON':
-                                //echo "encontré un patrón()";
-                                $wordToSearch = substr(strstr($words[$j], '('), 1, -1);
-                                $query .= $categoriasBusqueda[$i] . " LIKE '%" . $wordToSearch . "%'";  
-                                break;
-                            default:
-                                //echo "encontré una palabra";
-                                $query .= $categoriasBusqueda[$i] . " LIKE '%" . $words[$j] . "%'";
-                                break;
-                        }
-                        break;
-                }
-            }
-            echo $query . "<br/><br/>";
-            $results = executeQuery($query);
-            printResults($results);
+            structureQueryWithoutCampos($words);
         }
     }
 
